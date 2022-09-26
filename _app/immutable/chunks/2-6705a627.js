cc26dcb1adc5f26f2f5227bc3759f4f96fadef81
Daniele Pezzatini <pezzabros@gmail.com>
2022-09-26 19:17:33 +0200

    Updates

---
+++ _app/immutable/chunks/2-6705a627.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7ab0fbb3.js";export{t as component};
---
+++ _app/immutable/components/pages/_page.svelte-7ab0fbb3.js
@@ -0,0 +1,79 @@
+import{S as ma,i as pa,s as ba,k as t,q as n,a as g,l as a,m as o,r as i,h as s,c as m,n as r,H as wa,b as V,C as e,A as Pt}from"../../chunks/index-1f637bb8.js";function Ia(ga){let d,fe,ge,me,pe,be,we,Ie,S,Ee,ye,_e,ke,ce,v,L,Ce,Re,De,Be,Se,Te,Me,Ae,Ue,He,Fe,D,T,jt,Ne,x,Ve,de,l,G,Le,xe,M,Ge,Pe,A,je,Oe,ze,We,qe,b,P,Je,Ke,j,Qe,Xe,O,Ye,Ze,z,$e,et,U,tt,at,nt,it,st,H,rt,ot,he,E,W,lt,ct,f,y,q,dt,ht,F,vt,ut,ft,_,J,gt,mt,K,pt,bt,wt,k,Q,It,Et,X,yt,_t,kt,C,Y,Ct,Rt,Z,Dt,Bt,St,I,$,Tt,Mt,ee,At,Ut,te,Ht,Ft,Nt,R,ae,Vt,Lt,ne,xt,Gt;return{c(){d=t("section"),fe=n(`Hey there, I am Daniele.
+  `),ge=t("br"),me=t("br"),pe=n(`
+  For more than 10 years I've been helping companies and organizations shaping innovative ideas and data challanges into digital solutions that can clarify complexity, improve decision making or simply make somobody's life just a little bit better.
+  `),be=t("br"),we=t("br"),Ie=n(`
+
+  I like to consider myself a `),S=t("a"),Ee=n('"generalized specialist"'),ye=n(`, combining the skills of a software engineer with the mindset of a designer and a researcher.
+  `),_e=t("br"),ke=t("br"),ce=g(),v=t("section"),L=t("div"),Ce=n("What do i do"),Re=n(`
+
+	I am currently collaborating with Bestiario on data-driven innovation projects for companies and organizations,
+	such as United Nations University, Telefonica I+D, Satellogic and Seat. 
+	`),De=t("br"),Be=t("br"),Se=n(`
+	He has also taught in MA programs offered by the University of Florence and Universit\xE0 IUAV of Venice.
+	`),Te=t("br"),Me=t("br"),Ae=n(`
+
+	He has a strong interest in projects that use technology for social good. He has participated,
+	among others, in initiatives such as Data4Change and European Hub for Civic Engagement.
+
+	`),Ue=t("br"),He=t("br"),Fe=g(),D=t("figure"),T=t("img"),Ne=g(),x=t("figcaption"),Ve=n("Presenting during the European Hub for Civic Engagement workshop in Tallinn (\xA9Marek Metslaid)"),de=g(),l=t("section"),G=t("div"),Le=n("Research"),xe=n(`
+
+  I spent several formative years working as a
+	researcher at the `),M=t("a"),Ge=n("Media Integration and Communication Center (MICC)"),Pe=n(", in Florence first and at the "),A=t("a"),je=n("Interactive Technologies Group"),Oe=n(` in Universitat
+	Pompeu Fabra in Barcelona, where I was involved in several R&D projects experimenting innovative human-computer
+	interaction and multimedia analysis techniques in the field of cultural heritage, medicine, and smart
+	cities. 
+  `),ze=t("br"),We=t("br"),qe=n(`
+  Here are some of the research challanges I took on:
+
+  `),b=t("ul"),P=t("li"),Je=n("How can we use Natural Interaction and Virtualy Reality (VR) to improve training in emergency medicine?"),Ke=g(),j=t("li"),Qe=n("Can we use computer vision or other location sensors to enrich visitors experience in a museum?"),Xe=g(),O=t("li"),Ye=n('Can social network analysis help us discover the underlying "vibes" of a city?'),Ze=g(),z=t("li"),$e=n("Would a crowdsource approach help to improve video recommendation algorithims?"),et=n(`
+  
+  The work I have done with my teams has been published and presented in leading international conferences organized by ACM and IEEE, academic journals
+	and an `),U=t("a"),tt=n("edited book"),at=n(` published by Springer.
+  `),nt=t("br"),it=t("br"),st=n(` 
+  A full list of publication can be found on my `),H=t("a"),rt=n("Google Scholar profile"),ot=n("."),he=g(),E=t("section"),W=t("div"),lt=n("Teaching & Seminars"),ct=g(),f=t("ul"),y=t("li"),q=t("strong"),dt=n("IED - Barcelona"),ht=n(` - Coordinator of
+			`),F=t("a"),vt=n("Postgraduate Course in Data Driven Thinking"),ut=n(" (2022)"),ft=g(),_=t("li"),J=t("strong"),gt=n("Elisava - Barcelona"),mt=n(` - Workshop
+			`),K=t("i"),pt=n('"Designing the next interactive genome browser"'),bt=n(" - ChromDesign Winter school. (2020)"),wt=g(),k=t("li"),Q=t("strong"),It=n("IronHack - Barcelona"),Et=n(` - Talk
+			`),X=t("i"),yt=n('"Show me the data! - From big data to visual information"'),_t=n(" - Barcelona Data Week. (2019)"),kt=g(),C=t("li"),Y=t("strong"),Ct=n("IUAV University of Venice"),Rt=n(` - Teacher of
+			`),Z=t("i"),Dt=n('"Mobile app design and development with cross-platform tools"'),Bt=n(` - Master in Interactive Media
+			for Interior Design. (2016-2017)`),St=g(),I=t("li"),$=t("strong"),Tt=n("University of Florence"),Mt=n(" - Teacher of "),ee=t("i"),At=n('"Server side development with PHP"'),Ut=n(`
+			and `),te=t("i"),Ht=n('"Rich Internet Applications"'),Ft=n(" - Master in Multimedia Content Design. (2011 - 2014)"),Nt=g(),R=t("li"),ae=t("strong"),Vt=n("University of Florence"),Lt=n(` - Lecturer of
+			`),ne=t("i"),xt=n('"Multimedia Design and Development"'),Gt=n(" - BSc in Computer Science Engineering. (2011 - 2016)"),this.h()},l(c){d=a(c,"SECTION",{id:!0,class:!0});var h=o(d);fe=i(h,`Hey there, I am Daniele.
+  `),ge=a(h,"BR",{}),me=a(h,"BR",{}),pe=i(h,`
+  For more than 10 years I've been helping companies and organizations shaping innovative ideas and data challanges into digital solutions that can clarify complexity, improve decision making or simply make somobody's life just a little bit better.
+  `),be=a(h,"BR",{}),we=a(h,"BR",{}),Ie=i(h,`
+
+  I like to consider myself a `),S=a(h,"A",{href:!0,class:!0});var Ot=o(S);Ee=i(Ot,'"generalized specialist"'),Ot.forEach(s),ye=i(h,`, combining the skills of a software engineer with the mindset of a designer and a researcher.
+  `),_e=a(h,"BR",{}),ke=a(h,"BR",{}),h.forEach(s),ce=m(c),v=a(c,"SECTION",{id:!0,class:!0});var p=o(v);L=a(p,"DIV",{class:!0});var zt=o(L);Ce=i(zt,"What do i do"),zt.forEach(s),Re=i(p,`
+
+	I am currently collaborating with Bestiario on data-driven innovation projects for companies and organizations,
+	such as United Nations University, Telefonica I+D, Satellogic and Seat. 
+	`),De=a(p,"BR",{}),Be=a(p,"BR",{}),Se=i(p,`
+	He has also taught in MA programs offered by the University of Florence and Universit\xE0 IUAV of Venice.
+	`),Te=a(p,"BR",{}),Me=a(p,"BR",{}),Ae=i(p,`
+
+	He has a strong interest in projects that use technology for social good. He has participated,
+	among others, in initiatives such as Data4Change and European Hub for Civic Engagement.
+
+	`),Ue=a(p,"BR",{}),He=a(p,"BR",{}),Fe=m(p),D=a(p,"FIGURE",{class:!0});var ve=o(D);T=a(ve,"IMG",{src:!0,alt:!0,class:!0}),Ne=m(ve),x=a(ve,"FIGCAPTION",{class:!0});var Wt=o(x);Ve=i(Wt,"Presenting during the European Hub for Civic Engagement workshop in Tallinn (\xA9Marek Metslaid)"),Wt.forEach(s),ve.forEach(s),p.forEach(s),de=m(c),l=a(c,"SECTION",{id:!0,class:!0});var u=o(l);G=a(u,"DIV",{class:!0});var qt=o(G);Le=i(qt,"Research"),qt.forEach(s),xe=i(u,`
+
+  I spent several formative years working as a
+	researcher at the `),M=a(u,"A",{href:!0,class:!0});var Jt=o(M);Ge=i(Jt,"Media Integration and Communication Center (MICC)"),Jt.forEach(s),Pe=i(u,", in Florence first and at the "),A=a(u,"A",{href:!0,class:!0});var Kt=o(A);je=i(Kt,"Interactive Technologies Group"),Kt.forEach(s),Oe=i(u,` in Universitat
+	Pompeu Fabra in Barcelona, where I was involved in several R&D projects experimenting innovative human-computer
+	interaction and multimedia analysis techniques in the field of cultural heritage, medicine, and smart
+	cities. 
+  `),ze=a(u,"BR",{}),We=a(u,"BR",{}),qe=i(u,`
+  Here are some of the research challanges I took on:
+
+  `),b=a(u,"UL",{class:!0});var B=o(b);P=a(B,"LI",{class:!0});var Qt=o(P);Je=i(Qt,"How can we use Natural Interaction and Virtualy Reality (VR) to improve training in emergency medicine?"),Qt.forEach(s),Ke=m(B),j=a(B,"LI",{class:!0});var Xt=o(j);Qe=i(Xt,"Can we use computer vision or other location sensors to enrich visitors experience in a museum?"),Xt.forEach(s),Xe=m(B),O=a(B,"LI",{class:!0});var Yt=o(O);Ye=i(Yt,'Can social network analysis help us discover the underlying "vibes" of a city?'),Yt.forEach(s),Ze=m(B),z=a(B,"LI",{class:!0});var Zt=o(z);$e=i(Zt,"Would a crowdsource approach help to improve video recommendation algorithims?"),Zt.forEach(s),B.forEach(s),et=i(u,`
+  
+  The work I have done with my teams has been published and presented in leading international conferences organized by ACM and IEEE, academic journals
+	and an `),U=a(u,"A",{href:!0,class:!0});var $t=o(U);tt=i($t,"edited book"),$t.forEach(s),at=i(u,` published by Springer.
+  `),nt=a(u,"BR",{}),it=a(u,"BR",{}),st=i(u,` 
+  A full list of publication can be found on my `),H=a(u,"A",{href:!0,class:!0});var ea=o(H);rt=i(ea,"Google Scholar profile"),ea.forEach(s),ot=i(u,"."),u.forEach(s),he=m(c),E=a(c,"SECTION",{id:!0,class:!0});var ue=o(E);W=a(ue,"DIV",{class:!0});var ta=o(W);lt=i(ta,"Teaching & Seminars"),ta.forEach(s),ct=m(ue),f=a(ue,"UL",{class:!0});var w=o(f);y=a(w,"LI",{class:!0});var ie=o(y);q=a(ie,"STRONG",{});var aa=o(q);dt=i(aa,"IED - Barcelona"),aa.forEach(s),ht=i(ie,` - Coordinator of
+			`),F=a(ie,"A",{href:!0,class:!0});var na=o(F);vt=i(na,"Postgraduate Course in Data Driven Thinking"),na.forEach(s),ut=i(ie," (2022)"),ie.forEach(s),ft=m(w),_=a(w,"LI",{class:!0});var se=o(_);J=a(se,"STRONG",{});var ia=o(J);gt=i(ia,"Elisava - Barcelona"),ia.forEach(s),mt=i(se,` - Workshop
+			`),K=a(se,"I",{});var sa=o(K);pt=i(sa,'"Designing the next interactive genome browser"'),sa.forEach(s),bt=i(se," - ChromDesign Winter school. (2020)"),se.forEach(s),wt=m(w),k=a(w,"LI",{class:!0});var re=o(k);Q=a(re,"STRONG",{});var ra=o(Q);It=i(ra,"IronHack - Barcelona"),ra.forEach(s),Et=i(re,` - Talk
+			`),X=a(re,"I",{});var oa=o(X);yt=i(oa,'"Show me the data! - From big data to visual information"'),oa.forEach(s),_t=i(re," - Barcelona Data Week. (2019)"),re.forEach(s),kt=m(w),C=a(w,"LI",{class:!0});var oe=o(C);Y=a(oe,"STRONG",{});var la=o(Y);Ct=i(la,"IUAV University of Venice"),la.forEach(s),Rt=i(oe,` - Teacher of
+			`),Z=a(oe,"I",{});var ca=o(Z);Dt=i(ca,'"Mobile app design and development with cross-platform tools"'),ca.forEach(s),Bt=i(oe,` - Master in Interactive Media
+			for Interior Design. (2016-2017)`),oe.forEach(s),St=m(w),I=a(w,"LI",{class:!0});var N=o(I);$=a(N,"STRONG",{});var da=o($);Tt=i(da,"University of Florence"),da.forEach(s),Mt=i(N," - Teacher of "),ee=a(N,"I",{});var ha=o(ee);At=i(ha,'"Server side development with PHP"'),ha.forEach(s),Ut=i(N,`
+			and `),te=a(N,"I",{});var va=o(te);Ht=i(va,'"Rich Internet Applications"'),va.forEach(s),Ft=i(N," - Master in Multimedia Content Design. (2011 - 2014)"),N.forEach(s),Nt=m(w),R=a(w,"LI",{class:!0});var le=o(R);ae=a(le,"STRONG",{});var ua=o(ae);Vt=i(ua,"University of Florence"),ua.forEach(s),Lt=i(le,` - Lecturer of
+			`),ne=a(le,"I",{});var fa=o(ne);xt=i(fa,'"Multimedia Design and Development"'),fa.forEach(s),Gt=i(le," - BSc in Computer Science Engineering. (2011 - 2016)"),le.forEach(s),w.forEach(s),ue.forEach(s),this.h()},h(){r(S,"href","https://towardsdatascience.com/the-mindset-for-innovation-with-data-science-fc51605a4867"),r(S,"class","svelte-1e23sw"),r(d,"id","intro"),r(d,"class","svelte-1e23sw"),r(L,"class","section-title svelte-1e23sw"),wa(T.src,jt="img/daniele_intro.jpeg")||r(T,"src",jt),r(T,"alt","Daniele Pezzatini presenting"),r(T,"class","svelte-1e23sw"),r(x,"class","svelte-1e23sw"),r(D,"class","svelte-1e23sw"),r(v,"id","work"),r(v,"class","svelte-1e23sw"),r(G,"class","section-title svelte-1e23sw"),r(M,"href","http://www.micc.unifi.it/"),r(M,"class","svelte-1e23sw"),r(A,"href","https://www.upf.edu/web/gti"),r(A,"class","svelte-1e23sw"),r(P,"class","svelte-1e23sw"),r(j,"class","svelte-1e23sw"),r(O,"class","svelte-1e23sw"),r(z,"class","svelte-1e23sw"),r(b,"class","svelte-1e23sw"),r(U,"href","https://link.springer.com/book/10.1007/978-3-319-61036-8"),r(U,"class","svelte-1e23sw"),r(H,"href","https://scholar.google.it/citations?user=rt1N1i4AAAAJ&hl=en"),r(H,"class","svelte-1e23sw"),r(l,"id","research"),r(l,"class","svelte-1e23sw"),r(W,"class","section-title svelte-1e23sw"),r(F,"href","https://iedbarcelona.es/en/cursos-info/postgrado-en-data-driven-thinking/"),r(F,"class","svelte-1e23sw"),r(y,"class","svelte-1e23sw"),r(_,"class","svelte-1e23sw"),r(k,"class","svelte-1e23sw"),r(C,"class","svelte-1e23sw"),r(I,"class","svelte-1e23sw"),r(R,"class","svelte-1e23sw"),r(f,"class","svelte-1e23sw"),r(E,"id","teaching"),r(E,"class","svelte-1e23sw")},m(c,h){V(c,d,h),e(d,fe),e(d,ge),e(d,me),e(d,pe),e(d,be),e(d,we),e(d,Ie),e(d,S),e(S,Ee),e(d,ye),e(d,_e),e(d,ke),V(c,ce,h),V(c,v,h),e(v,L),e(L,Ce),e(v,Re),e(v,De),e(v,Be),e(v,Se),e(v,Te),e(v,Me),e(v,Ae),e(v,Ue),e(v,He),e(v,Fe),e(v,D),e(D,T),e(D,Ne),e(D,x),e(x,Ve),V(c,de,h),V(c,l,h),e(l,G),e(G,Le),e(l,xe),e(l,M),e(M,Ge),e(l,Pe),e(l,A),e(A,je),e(l,Oe),e(l,ze),e(l,We),e(l,qe),e(l,b),e(b,P),e(P,Je),e(b,Ke),e(b,j),e(j,Qe),e(b,Xe),e(b,O),e(O,Ye),e(b,Ze),e(b,z),e(z,$e),e(l,et),e(l,U),e(U,tt),e(l,at),e(l,nt),e(l,it),e(l,st),e(l,H),e(H,rt),e(l,ot),V(c,he,h),V(c,E,h),e(E,W),e(W,lt),e(E,ct),e(E,f),e(f,y),e(y,q),e(q,dt),e(y,ht),e(y,F),e(F,vt),e(y,ut),e(f,ft),e(f,_),e(_,J),e(J,gt),e(_,mt),e(_,K),e(K,pt),e(_,bt),e(f,wt),e(f,k),e(k,Q),e(Q,It),e(k,Et),e(k,X),e(X,yt),e(k,_t),e(f,kt),e(f,C),e(C,Y),e(Y,Ct),e(C,Rt),e(C,Z),e(Z,Dt),e(C,Bt),e(f,St),e(f,I),e(I,$),e($,Tt),e(I,Mt),e(I,ee),e(ee,At),e(I,Ut),e(I,te),e(te,Ht),e(I,Ft),e(f,Nt),e(f,R),e(R,ae),e(ae,Vt),e(R,Lt),e(R,ne),e(ne,xt),e(R,Gt)},p:Pt,i:Pt,o:Pt,d(c){c&&s(d),c&&s(ce),c&&s(v),c&&s(de),c&&s(l),c&&s(he),c&&s(E)}}}class ya extends ma{constructor(d){super(),pa(this,d,null,Ia,ba,{})}}export{ya as default};
