ce4daac168cf11688bbc8a548a931db43123ddcc
Daniele Pezzatini <pezzabros@gmail.com>
2022-09-26 19:48:26 +0200

    Updates

--- _app/immutable/chunks/1-f0aa99b3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-4d141eae.js";export{t as component};
--- _app/immutable/chunks/2-4ae5e8de.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-74ef3d67.js";export{t as component};
