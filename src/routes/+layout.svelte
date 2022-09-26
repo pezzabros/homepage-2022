40a648bae0bd3064e9c1cb940a9c8473d60bc1a7
Daniele Pezzatini <daniele@bestiario.org>
2022-09-26 18:51:41 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<div class="site">
+    <header>
+      <h1>Daniele Pezzatini</h1>
+      
+      <nav>
+        <a href="#work">WORK</a> / 
+        <a href="#research">Research</a> /
+        <a href="#teaching">Teaching</a> 
+      </nav>
+    </header>
+    <div class="main">
+      <slot />
+    </div>
+    
+</div>
+
+<style>
+  :global(body) {
+    background-color: #fff;
+    color: #333;
+    font-family: 'Lora', serif;
+    
+    margin: 0px;
+    padding: 25px;
+  }
+  .site {
+    margin: 40px auto;
+		max-width: 820px;
+  }
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 26px;
+    letter-spacing: 0.03em;
+    /*color: #624d75*/
+
+  }
+
+  header {
+    margin-bottom: 100px;
+    display: flex;
+    align-items: center;
+  }
+  nav{
+    margin-left: auto
+  }
+
+  nav a {
+    text-decoration: none;
+    color: black;
+    font-family: 'Poppins';
+    font-weight: 300;
+    text-transform: uppercase;
+  }
+
+  nav a:hover {
+    text-decoration: underline;
+  }
+
+
+  .main {
+    font-size: 1.1em;
+    line-height: 1.5em;
+    letter-spacing: 0.025em;
+  }
+  
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,162 @@
+<section id="intro">
+	Hey there, I am Daniele.
+  <br><br>
+  For more than 10 years I've been helping companies and organizations shaping innovative ideas and data challanges into digital solutions that can clarify complexity, improve decision making or simply make somobody's life just a little bit better.
+  <br><br>
+
+  I like to consider myself a <a href="https://towardsdatascience.com/the-mindset-for-innovation-with-data-science-fc51605a4867">"generalized specialist"</a>, combining the skills of a software engineer with the mindset of a designer and a researcher.
+  <br><br>
+
+  <!--<div class="contacts">
+    <a href="#"><div class="icon mail"></div></a>
+    <a href="#"><div class="icon twitter"></div></a>
+    <a href="#"><div class="icon linkedin"></div></a>
+    <a href="#"><div class="icon github"></div></a>
+  </div>-->
+
+</section>
+
+<section id="work">
+
+  <div class="section-title">What do i do</div>
+
+	I am currently collaborating with Bestiario on data-driven innovation projects for companies and organizations,
+	such as United Nations University, Telefonica I+D, Satellogic and Seat. 
+	<br /><br />
+	He has also taught in MA programs offered by the University of Florence and Università IUAV of Venice.
+	<br /><br />
+
+	He has a strong interest in projects that use technology for social good. He has participated,
+	among others, in initiatives such as Data4Change and European Hub for Civic Engagement.
+
+	<br /><br />
+
+	<figure>
+		<img src="img/daniele_intro.jpeg" alt="Daniele Pezzatini presenting" />
+		<figcaption>
+			Presenting during the European Hub for Civic Engagement workshop in Tallinn (©Marek Metslaid)
+		</figcaption>
+	</figure>
+</section>
+
+<section id="research">
+	<div class="section-title">Research</div>
+
+  I spent several formative years working as a
+	researcher at the <a href="http://www.micc.unifi.it/">Media Integration and Communication Center (MICC)</a>, in Florence first and at the <a href="https://www.upf.edu/web/gti">Interactive Technologies Group</a> in Universitat
+	Pompeu Fabra in Barcelona, where I was involved in several R&D projects experimenting innovative human-computer
+	interaction and multimedia analysis techniques in the field of cultural heritage, medicine, and smart
+	cities. 
+  <br><br>
+  Here are some of the research challanges I took on:
+
+  <ul>
+		<li>How can we use Natural Interaction and Virtualy Reality (VR) to improve training in emergency medicine?</li>
+    <li>Can we use computer vision or other location sensors to enrich visitors experience in a museum?</li>
+    <li>Can social network analysis help us discover the underlying "vibes" of a city?</li>
+    <li>Would a crowdsource approach help to improve video recommendation algorithims?</li>
+
+  </ul>
+  
+  The work I have done with my teams has been published and presented in leading international conferences organized by ACM and IEEE, academic journals
+	and an <a href="https://link.springer.com/book/10.1007/978-3-319-61036-8">edited book</a> published by Springer.
+  <br><br> 
+  A full list of publication can be found on my <a href="https://scholar.google.it/citations?user=rt1N1i4AAAAJ&hl=en">Google Scholar profile</a>.
+</section>
+
+
+<section id="teaching">
+	<div class="section-title">Teaching & Seminars</div>
+
+	<ul>
+		<li>
+			<strong>IED - Barcelona</strong> - Coordinator of
+			<a href="https://iedbarcelona.es/en/cursos-info/postgrado-en-data-driven-thinking/"
+				>Postgraduate Course in Data Driven Thinking</a
+			> (2022)
+		</li>
+		<li>
+			<strong>Elisava - Barcelona</strong> - Workshop
+			<i>"Designing the next interactive genome browser"</i> - ChromDesign Winter school. (2020)
+		</li>
+		<li>
+			<strong>IronHack - Barcelona</strong> - Talk
+			<i>"Show me the data! - From big data to visual information"</i> - Barcelona Data Week. (2019)
+		</li>
+		<li>
+			<strong>IUAV University of Venice</strong> - Teacher of
+			<i>"Mobile app design and development with cross-platform tools"</i> - Master in Interactive Media
+			for Interior Design. (2016-2017)
+		</li>
+		<li>
+			<strong>University of Florence</strong> - Teacher of <i>"Server side development with PHP"</i>
+			and <i>"Rich Internet Applications"</i> - Master in Multimedia Content Design. (2011 - 2014)
+		</li>
+		<li>
+			<strong>University of Florence</strong> - Lecturer of
+			<i>"Multimedia Design and Development"</i> - BSc in Computer Science Engineering. (2011 - 2016)
+		</li>
+	</ul>
+</section>
+
+<style>
+	section {
+		text-align: justify;
+		margin-bottom: 50px;
+	}
+
+	.section-title {
+		font-family: 'Poppins';
+		font-weight: 400;
+		font-size: 20px;
+		text-transform: uppercase;
+		margin-bottom: 15px;
+	}
+
+	ul li {
+		margin-bottom: 1em;
+	}
+
+	a {
+		color: #971f1f;
+		text-decoration: none;
+	}
+
+  figure {
+    margin: 0px;
+  }
+	figure img{
+		width: 100%;
+	}
+
+  figure figcaption{
+		font-size: 0.7em;
+	}
+  .contacts {
+    display: flex;
+    align-items: center;
+  }
+
+  .contacts .icon{
+    display: block;
+    width: 20px;
+    height: 20px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    margin-right: 20px;
+  }
+
+  .icon.mail {
+    background-image: url('icons/mail.png')
+  }
+  .icon.twitter {
+    background-image: url('icons/twitter.png')
+  }
+  .icon.linkedin {
+    background-image: url('icons/linkedin.png')
+  }
+  .icon.github {
+    background-image: url('icons/github.png')
+  }
+</style>
